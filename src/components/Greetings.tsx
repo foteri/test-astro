0a32fdc7a1ebbff18978db1428f61f73b32a02cc
f.oteri <f.oteri@accenture.com>
2025-04-24 10:26:20 +0200

    useless class removed

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -7,7 +7,7 @@ export default function Greeting({messages}: any) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div class={'test'}>
+    <div>
       <h3>{greeting}! Thank you for visiting!</h3>
       <button onClick={() => setGreeting(randomMessage())}>
         New Greeting
